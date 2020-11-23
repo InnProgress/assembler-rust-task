a32de10db3a8a0a5fe0bfce6f87a34ee685a15ad
InnProgress <arnoldautuch4@gmail.com>
2020-11-23 10:59:13 +0200

    feat: filter out non primitive pythagorean triples in assembly before outputing to file

--- src/pythagorean_numbers.rs
+++ src/pythagorean_numbers.rs
@@ -7,7 +7,7 @@ pub fn run() {
     let mut file = File::create("results.csv").unwrap();
     file.write(b"X,Y,Z\n").unwrap();
 
-    let mut pythagorean_numbers = vec![];
+    let mut pythagorean_numbers: Vec<(i32, i32)> = vec![];
 
     for i in 1..=996 {
         let t1 = f32x4([i as f32; 4]);
@@ -56,10 +56,52 @@ pub fn run() {
                     out("xmm4") _, out("xmm5") _
                 )
             }
+
+            for l in 0..pythagorean_numbers.len() {
+                for k in 0..4 {
+                    let value: i32;
+                    unsafe {
+                        asm!(
+                            "
+                            MOV edx, 0
+                            DIV ecx
+
+                            CMP edx, 0
+                            JNE STOP
+
+                            MOV edi, eax
+                            MOV eax, esi
+                            MOV edx, 0
+                            DIV ebx
+
+                            CMP edx, 0
+                            JNE STOP
+
+                            CMP eax, edi
+                            JNE STOP
+                            MOV eax, 1
+                            JMP END
+
+                            STOP:
+                            MOV eax, 0
+
+                            END:
+                        ",
+                            inout("eax") t1.0[k] as i32 => value,
+                            in("esi") t2.0[k] as i32,
+                            in("ecx") pythagorean_numbers[l].0,
+                            in("ebx") pythagorean_numbers[l].1,
+                        );
+                    };
+                    if value == 1 {
+                        output.0[k] = 1.;
+                    }
+                }
+            }
+
             for l in 0..4 {
-                // FILTER
                 if output.0[l] == 0. {
-                    pythagorean_numbers.push([t1.0[l], t2.0[l]]);
+                    pythagorean_numbers.push((t1.0[l] as i32, t2.0[l] as i32));
                     file.write(format!("{},{},{}\n", t1.0[l], t2.0[l], z_values.0[l]).as_bytes())
                         .unwrap();
                 }
@@ -67,5 +109,5 @@ pub fn run() {
         }
     }
 
-    println!("Pythagorean numbers calculated and outputed to results.txt");
+    println!("Pythagorean numbers calculated and outputed to results.csv");
 }
